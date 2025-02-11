9056873d1d2e124555fc35ad3c367f21c4a8972a
Dhruva Sharma <dhruva1098@gmail.com>
2025-02-11 13:35:04 +0530

    minor changes to function definitations, comply to cpplint

--- Headers/Tensor.h
+++ Headers/Tensor.h
@@ -1,35 +1,40 @@
-//
-// Created by Dhruva Sharma on 10/2/25.
-//
+// Copyright 2025 Dhruva Sharma
 
-#ifndef TENSOR_CLASS_H
-#define TENSOR_CLASS_H
+#ifndef HEADERS_TENSOR_H_
+#define HEADERS_TENSOR_H_
+
 #include <vector>
 
 class Tensor {
   // No templates type, will use double throughout
   // Constructor, vector<int>;vector<int>(2)
-
-  Tensor(std::vector<size_t>& shape);  // Tensor t1({1,1,1}) // all garbage values
-  Tensor(std::vector<size_t>& shape, double* initial_value);  // Tensor t1({1,1,1}, 40})
+  explicit Tensor(const std::vector<size_t>& shape);  // Tensor t1({1,1,1}) // all garbage values
+  Tensor(const std::vector<size_t>& shape, double* initialValue);  // Tensor t1({1,1,1}, 40})
 
   // destructor
   ~Tensor();
 
-  // accessors: shape, dim
-  std::vector<size_t> shape(const Tensor& tensor) const;
-  std::vector<size_t> dim(const std::vector<size_t>& tensor_internal) const;
-  // arr[3]
-  // operator overload [] (single dimensional tensor)
-  double operator[](const size_t tensor_index) const;
+  // accessors: shape, dim, size
+  size_t size() const;
+  size_t dim(size_t index) const;
+  std::vector<size_t> shape() const;
+
+  // element access [] (single dimensional tensor)
+  double& operator[](size_t index);  // Creating different for both const and non const
+  const double& operator[](size_t index) const;
   // operator overlad () (multi dimensional tensor)
-  double& operator[](const std::vector<size_t>& tensor_index); const
+  double& operator()(const std::vector<size_t>& indices);
+  const double& operator()(const std::vector<size_t>& indices) const;
   // op overload +, -, /, *
-  Tensor operator+(const Tensor& other) const; // t1 + t2 == t1(operator+(t2))
-  // private
-  // check_dim
-  // calculate_index arr[6] == arr + 6*sizeof(int);
+  Tensor operator+(const Tensor& other) const;  // t1 + t2 == t1(operator+(t2))
+
+ private:
+    std::vector<size_t> _shape;
+    std::vector<double> _data;
+    size_t computeIndex(const std::vector<size_t>& indices) const;
+    bool checkShapeCompatibility(const Tensor& other) const;
 };
-#endif //TENSOR_CLASS_H
+
+#endif  // HEADERS_TENSOR_H_
 
 
