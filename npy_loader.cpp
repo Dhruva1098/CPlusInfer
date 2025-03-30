3ae7886aafea34036dae183d515848a6ccaa2cf3
Dhruva Sharma <dhruva1098@gmail.com>
2025-03-30 23:08:49 +0530

    created cpp for npy_loader:version check

--- npy_loader.cpp
+++ npy_loader.cpp
@@ -14,4 +14,54 @@ std::map<std::string, std::string> NPYParser::parse_header(std::istream& file) {
   if (std::string(magic, 6) != "\x93NUMPY") {
     throw std::runtime_error("Unvalid NPY file format");
   }
+
+  // read version 
+  uint8_t major_version, minor_version;
+  file.read(reinterpret_cast<char*>(&major_version), 1);
+  file.read(reinterpret_cast<char*>(&minor_version), 1);
+
+  // store version
+  metadata["major_version"] = std::to_string(major_version);
+  metadata["minor_version"] = std::to_string(minor_version);
+
+  // read header length
+  uint16_t header_len = 0;
+  uint32_t header_len_big = 0;
+  
+  if (major_version == 1) {
+    file.read(reinterpret_cast<char*>(&header_len), 2);
+  } else if (major_version == 2){
+    file.read(reinterpret_cast<char*>(&header_len_big), 4);
+    header_len = static_cast<uint16_t>(header_len_big);
+  } else {
+    throw std::runtime_error("Unsupported NPY version " + std::to_string(major_version));
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
